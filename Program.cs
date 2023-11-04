d1fbfd28000b501109b16585b0339be0a4c03854
marcio592 <marciosantos592@gmail.com>
2023-11-03 22:17:48 -0300

    Adição de comentário output

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ pessoa.setIdade=28;
 pessoa.setAltura=1.70;
 pessoa.setSalario=3500M;
 
+//adição de nova linha output
 Console.WriteLine($"Olá, meu nome é {pessoa.getNome}, tenho {pessoa.getIdade} anos e");
 Console.WriteLine($"minha altura é {pessoa.getAltura.ToString("0.00")} cm. Meu salário é R$ {pessoa.getSalario}");
 
