0d16f97623cc7ab8828a559f1844bad699b9e44f
marcio592 <marciosantos592@gmail.com>
2023-11-04 16:43:51 -0300

    Convertendo tipo de variável

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ pessoa.setNome="João";
 pessoa.setIdade=28;
 pessoa.setAltura=1.70;
 pessoa.setSalario=3500M;
-pessoa.setCpf= Convert.ToInt32("11111111"); //Utilizando converção de tipo de variável
+pessoa.setCpf= Convert.ToInt32("11111111"); //Convertendo   tipo de variável
 
 //adição de nova linha output
 Console.WriteLine($"Olá, meu nome é {pessoa.getNome}, tenho {pessoa.getIdade} anos e");
