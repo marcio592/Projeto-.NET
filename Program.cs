e5b7bfdc1a91232724575057aefb913329309a24
marcio592 <marciosantos592@gmail.com>
2023-11-03 21:38:31 -0300

    adição de comentários de linha

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿using Projeto_.NET.Models;
 
-
+//criação do objeto pessoa
 Pessoa pessoa = new Pessoa("", 0);
 
 pessoa.setNome="João";
 pessoa.setIdade=28;
 
 Console.WriteLine($"Olá, meu nome é {pessoa.getNome} e tenho {pessoa.getIdade} anos");
+
 
