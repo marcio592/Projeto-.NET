39946de777b1603a87c0627861c92a497f7287f2
marcio592 <marciosantos592@gmail.com>
2023-11-03 22:34:14 -0300

    inclusão de nova linha

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
 //criação do objeto pessoa
 Pessoa pessoa = new Pessoa("", 0, 0, 0);
 
+DateTime dataHoraAtual = DateTime.Now;
+
 pessoa.setNome="João";
 pessoa.setIdade=28;
 pessoa.setAltura=1.70;
@@ -11,5 +13,6 @@ pessoa.setSalario=3500M;
 //adição de nova linha output
 Console.WriteLine($"Olá, meu nome é {pessoa.getNome}, tenho {pessoa.getIdade} anos e");
 Console.WriteLine($"minha altura é {pessoa.getAltura.ToString("0.00")} cm. Meu salário é R$ {pessoa.getSalario}");
+Console.WriteLine($"Data e hora do output: {dataHoraAtual}");
 
 
