6364f6a0055a224f53359628ee6cd51d1c74dc39
marcio592 <marciosantos592@gmail.com>
2023-11-03 22:13:39 -0300

    Adição de novas linhas de código(variáveis)

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -9,11 +9,15 @@ namespace Projeto_.NET.Models
     {
         private String nome;
         private int idade;
+        private double altura;
+        private decimal salario;
 
-        public Pessoa(String nome, int idade)
+        public Pessoa(String nome, int idade , double altura, decimal salario)
         {
             this.nome=nome;
             this.idade=idade;
+            this.altura=altura;
+            this.salario=salario;
 
 
         }
@@ -31,6 +35,18 @@ namespace Projeto_.NET.Models
         }
         public int setIdade{
             set{idade=value;}
+        }
+        public double getAltura{
+            get{return altura;}
+        }
+        public double setAltura{
+            set{altura=value;}
+        }
+        public decimal getSalario{
+            get{return salario;}
+        }
+        public decimal setSalario{
+            set{salario=value;}
         }
 
         
